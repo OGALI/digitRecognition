05111e1d80f295950b1f3aad86a73fdee56bd75a
Ali Najmaldin <ali.najmaldin@gmail.com>
2019-05-26 09:18:23 -0400

    added the data and dataLoader and minist_loader.py

--- src/Network.py
+++ src/Network.py
@@ -81,6 +81,8 @@ class Network:
                 # For each minibatch, apply single step of the gradient descent and updates weights and biases
                 # use 1 mini batch to calculate gradient
                 self.update_mini_batch(mini_batch, eta)
+
+            # ***explain these line here
             if test_data:
                 print
                 "Epoch {0}: {1} / {2}".format(
@@ -94,8 +96,10 @@ class Network:
                 gradient descent using backpropagation to a single mini batch.
                 The "mini_batch" is a list of tuples "(x, y)", and "eta"
                 is the learning rate."""
+        # The training_data is a list of tuples (x, y) representing the training inputs and corresponding desired outputs.
 
         # Initialize the gradients to zero
+        # ***why gradient starts at zero every time
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
 
@@ -103,22 +107,77 @@ class Network:
 
             # ***why nibla is calculated for each point
             # calculates the delta to be used required
+            # calculates the delta to be used for each point and updates nabla_b/w
             delta_nabla_b, delta_nabla_w = self.backprop(x, y)
 
             # adding the delta to the gradients to get new gradient
+            # this is storing an array of nb gradients each iteration so then can average them
+            # since gradient starts at zero, delta tells where to go from 0
             nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
             nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
 
         self.weights = [w - (eta / len(mini_batch)) * nw for w, nw in zip(self.weights, nabla_w)]
         self.biases = [b - (eta / len(mini_batch)) * nb for b, nb in zip(self.biases, nabla_b)]
 
+
+
     def backprop(self, x, y):
         """Return a tuple ``(nabla_b, nabla_w)`` representing the
         gradient for the cost function C_x.  ``nabla_b`` and
         ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
         to ``self.biases`` and ``self.weights``."""
+        # The training_data is a list of tuples (x, y) representing the training inputs and corresponding desired outputs.
 
-        pass
+        # Initialize the gradients to 0
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+
+        # feedforward
+        activation = x
+        activations = [x] # list to store all the activations, layer by layer
+        zs = [] # list to store all the z vectors, layer by layer
+
+        # iterates over the list of biases and weights 1 by 1 and make list of activations and z
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation)+b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+
+        # backward pass
+        delta = self.cost_derivative(activations[-1], y) * \
+            sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+        # Note that the variable l in the loop below is used a little
+        # differently to the notation in Chapter 2 of the book.  Here,
+        # l = 1 means the last layer of neurons, l = 2 is the
+        # second-last layer, and so on.  It's a renumbering of the
+        # scheme in the book, used here to take advantage of the fact
+        # that Python can use negative indices in lists.
+        for l in range(2, self.num_layers):
+            z = zs[-l]
+            sp = sigmoid_prime(z)
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+        return (nabla_b, nabla_w)
+
+
+    def evaluate(self, test_data):
+        """Return the number of test inputs for which the neural
+        network outputs the correct result. Note that the neural
+        network's output is assumed to be the index of whichever
+        neuron in the final layer has the highest activation."""
+        test_results = [(np.argmax(self.feedforward(x)), y)
+                        for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_results)
+
+    def cost_derivative(self, output_activations, y):
+        """Return the vector of partial derivatives \partial C_x /
+        \partial a for the output activations."""
+        return (output_activations-y)
 
 
 # Required function to evaluate
